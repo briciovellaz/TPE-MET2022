d5fff8d28a2a2cb0a34bc62f5d67c07607ac9871
Bricio Vellaz <briciovellaz@gmail.com>
2022-06-04 15:15:17 -0300

    Agregue metodos listarTurnos y cargarHorarios a secretaria

--- TurnoFacilMetodologia2022/src/com/company/Secretaria.java
+++ TurnoFacilMetodologia2022/src/com/company/Secretaria.java
@@ -1,6 +1,8 @@
 package com.company;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Secretaria extends Profesional{
     private ArrayList<Medico> medicos = new ArrayList<Medico>();
@@ -11,5 +13,25 @@ public class Secretaria extends Profesional{
 
     public void addMedico(Medico m){
         medicos.add(m);
+    }
+
+    public List<Turno> listarTurnos(){
+        List<Turno> salida=new ArrayList<>();
+        for(Medico m: medicos){
+            for(int i=0;i<=7;i++){
+            salida.addAll(m.listarTurnos(i));
+            }
+        }
+        return salida;
+    }
+
+    public void cargarHorarios(Medico m, LocalTime horaInicio, LocalTime horaFin,int duracion, int...dias){
+        if(medicos.contains(m)){
+            int i=0;
+            while(i< medicos.size()&&!medicos.get(i).equals(m)){i++;}
+            for(int d:dias){
+                medicos.get(i).agregarHorario(d,horaInicio,horaFin,duracion);
+            }
+        }
     }
 }
